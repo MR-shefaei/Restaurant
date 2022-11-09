f7660566dca739cc3c7e724da87ef719b4927247
MohammadReza <shefaei313@gmail.com>
2022-11-09 11:38:31 +0430

    added headar and footer

---
+++ components/header.js
@@ -0,0 +1,13 @@
+
+
+export default function Header(){
+    return(
+        <>
+            <div className="bg-[#FFFCFA] flex justify-center gap-28 p-8">
+                <a href="#">Restaurant</a>
+                <a href="#">About</a>
+            </div>
+        
+        </>
+    );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import Footer from '../components/footer';
+import Header from '../components/header';
 import '../styles/globals.css';
 
 
 
 export default function app(){
     return(
+        <>
+        <Header/>
         <Footer/>
+        </>
     )
 }
