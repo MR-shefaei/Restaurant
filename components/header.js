a39ff5cbb869f07e6bdcb54d72748fd26a5afe57
MohammadReza <shefaei313@gmail.com>
2022-11-09 12:02:19 +0430

    brought some changes into header component

--- components/header.js
+++ components/header.js
@@ -1,13 +1,29 @@
+import { useState } from "react";
 
+export default function Header() {
 
-export default function Header(){
-    return(
-        <>
-            <div className="bg-[#FFFCFA] flex justify-center gap-28 p-8">
-                <a href="#">Restaurant</a>
-                <a href="#">About</a>
-            </div>
-        
-        </>
-    );
+    const [useStatus , setStatus] = useState(1);
+    function setStatus(e){
+        setStatus(1);
+    }
+  return (
+
+    <>
+      <div className="bg-[#FFFCFA] flex justify-center  p-8   font-semibold ">
+        <a
+          onClick={setstaus(1)}
+          className="mx-28 hover:text-indigo-900 text-gray-300 hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 hover:decoration-solid "
+          href="#"
+        >
+          Restaurant
+        </a>
+        <a
+          className="mx-28 hover:text-indigo-900 text-gray-300 hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 hover:decoration-solid"
+          href="#"
+        >
+          About Us
+        </a>
+      </div>
+    </>
+  );
 }
