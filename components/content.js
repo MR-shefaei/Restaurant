5f7023a2772837b0a1519c3286647f20f109aa34
MohammadReza <shefaei313@gmail.com>
2022-11-10 10:52:00 +0430

    solve the problem of usestate

--- components/content.js
+++ components/content.js
@@ -3,12 +3,15 @@ import React from "react";
 export default function Content(){
     return(
         <>
-        <div className="grid grid-cols-3">
-            <div className="col-span-1">
-                
-            </div>
+        <div className="grid grid-cols-7">
             <div className="col-span-2">
-
+                <div className="">
+                    <h2>Category</h2>
+                    
+                </div>
+            </div>
+            <div className="col-span-5">
+                content
             </div>
 
             
--- components/header.js
+++ components/header.js
@@ -1,32 +1,33 @@
 import { useState } from "react";
 
 export default function Header() {
-  const [useStyle, setStyle] = useState(0);
+  const [useStyle, setStyle] = useState(1);
 
-  function eventHandler() {
-    setStyle(1);
+  function eventHandler(e) {
+    
+    setStyle(e);
   }
 
   return (
     <>
       <div className="bg-[#FFFCFA] flex justify-center  p-8   font-semibold ">
         <a
-          onClick={() => eventHandler()}
-          className={`mx-28  hover:text-indigo-900 text-gray-300 ${
+          onClick={() => eventHandler(1)}
+          className={`mx-28  text-gray-300 ${
             useStyle === 1
-              ? "  hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 decoration-solid"
-              : ""
+              ? "  underline decoration-2 underline-offset-[38px] decoration-pink-300 decoration-solid text-indigo-900"
+              : "hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 decoration-solid"
           } `}
           href="#"
         >
           Restaurant
         </a>
         <a
-          onClick={() => eventHandler()}
+          onClick={() => eventHandler(2)}
           className={`mx-28 hover:text-indigo-900 text-gray-300 ${
-            useStyle === 1
-              ? "  hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 decoration-solid"
-              : ""
+            useStyle === 2
+              ? "  underline decoration-2 underline-offset-[38px] decoration-pink-300 decoration-solid text-indigo-900"
+              : "hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 decoration-solid"
           }`}
           href="#"
         >
