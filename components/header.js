3863be3cf6177a0b10263ed25b3d55e5e4ffbdf9
MohammadReza <shefaei313@gmail.com>
2022-11-10 09:45:51 +0430

    working on header component

--- components/header.js
+++ components/header.js
@@ -1,24 +1,26 @@
-import { useState } from "react";
+import {  useState } from "react";
 
 export default function Header() {
 
-    const [useStatus , setStatus] = useState(1);
-    function setStatus(e){
-        setStatus(1);
+    const [useStyle , setStyle] = useState(0);
+    
+    function eventHandler(e){
+       setStyle(e);
     }
   return (
 
     <>
       <div className="bg-[#FFFCFA] flex justify-center  p-8   font-semibold ">
         <a
-          onClick={setstaus(1)}
-          className="mx-28 hover:text-indigo-900 text-gray-300 hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 hover:decoration-solid "
+          onClick={()=>eventHandler(1)}
+          className={`mx-28 ${useStyle === 1 ? " hover:text-indigo-900 text-gray-300 hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 hover:decoration-solid" : ""} `}
           href="#"
         >
           Restaurant
         </a>
         <a
-          className="mx-28 hover:text-indigo-900 text-gray-300 hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 hover:decoration-solid"
+          onClick={()=>eventHandler(1)}
+          className={`mx-28 ${useStyle === 1 ? " hover:text-indigo-900 text-gray-300 hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 hover:decoration-solid" : ""}`}
           href="#"
         >
           About Us
