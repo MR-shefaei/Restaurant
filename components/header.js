bd65857065ea966fcfc7b2e694bff12406c7f0e4
MohammadReza <shefaei313@gmail.com>
2022-11-10 10:15:38 +0430

    working on footer component

--- components/header.js
+++ components/header.js
@@ -1,26 +1,33 @@
-import {  useState } from "react";
+import { useState } from "react";
 
 export default function Header() {
+  const [useStyle, setStyle] = useState(0);
 
-    const [useStyle , setStyle] = useState(0);
-    
-    function eventHandler(e){
-       setStyle(e);
-    }
+  function eventHandler() {
+    setStyle(1);
+  }
+
   return (
-
     <>
       <div className="bg-[#FFFCFA] flex justify-center  p-8   font-semibold ">
         <a
-          onClick={()=>eventHandler(1)}
-          className={`mx-28 ${useStyle === 1 ? " hover:text-indigo-900 text-gray-300 hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 hover:decoration-solid" : ""} `}
+          onClick={() => eventHandler()}
+          className={`mx-28  hover:text-indigo-900 text-gray-300 ${
+            useStyle === 1
+              ? "  hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 decoration-solid"
+              : ""
+          } `}
           href="#"
         >
           Restaurant
         </a>
         <a
-          onClick={()=>eventHandler(1)}
-          className={`mx-28 ${useStyle === 1 ? " hover:text-indigo-900 text-gray-300 hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 hover:decoration-solid" : ""}`}
+          onClick={() => eventHandler()}
+          className={`mx-28 hover:text-indigo-900 text-gray-300 ${
+            useStyle === 1
+              ? "  hover:underline decoration-2 underline-offset-[38px] decoration-pink-300 decoration-solid"
+              : ""
+          }`}
           href="#"
         >
           About Us
