3482dd25300b34744e66e895049faa1ccc2795a5
MohammadReza <shefaei313@gmail.com>
2022-11-13 11:17:19 +0430

    connected with backend

--- components/content.js
+++ components/content.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {API_URL} from "../config"
 
 export default function Content(){
+console.log(API_URL, "this");  
+useEffect(async ()=>{
+    const categories = await fetch(`${API_URL}/api/categories`);
+    const response = await categories.json();
+    console.log(response);
+})
     return(
         <>
-        <div className="grid grid-cols-7">
+        <div className="grid grid-cols-7 container mx-auto">
             <div className="col-span-2">
                 <div className="">
-                    <h2>Category</h2>
+                    <h2 className="font-bold pb-5">Categories</h2>
+                    <ul className="">
+                        <li>
+                            All
+                        </li>
+                        <li>
+                            French
+                        </li>
+                    </ul>
                     
                 </div>
             </div>
---
+++ config/index.js
@@ -0,0 +1,2 @@
+const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337"
+export {API_URL}
